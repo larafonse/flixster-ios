eb9ff59f18b786b82293981df3f46dddbf85b633
Nicolas Lara Fonseca <53790517+larafonse@users.noreply.github.com>
2022-02-08 21:29:29 -0800

    Initial Commit

---
+++ FlixsteriOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixsteriOS
+//
+//  Created by Nicolas Lara on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
